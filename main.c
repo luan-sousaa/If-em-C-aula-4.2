37d41aef24945faf7692972c8aa2b20ed82cb58d
LUAN <110142201+LluanBDS@users.noreply.github.com>
2024-06-06 13:18:29 -0300

    main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float m ;
+    printf("digite a sua nota:");
+    scanf("%f", &m);
+    if (m >= 7){
+        printf("aprovado");
+    } else{
+        printf("reprovado");
+    }
+    
+}
